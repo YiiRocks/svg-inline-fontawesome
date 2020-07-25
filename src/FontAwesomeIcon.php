e200978974288d474c223c5577d9bbc52cf77892
Mr.42 <johan@mr42.me>
2020-07-25 03:27:12 +0200

    Remove obsolete `$style`

--- src/FontAwesomeIcon.php
+++ src/FontAwesomeIcon.php
@@ -12,11 +12,6 @@ final class FontAwesomeIcon extends \YiiRocks\SvgInline\Icon
     /** @var bool Set to `true` to have a fixed width icon */
     protected bool $fixedWidth;
 
-    /**
-     * @var string Style of the icon, must match `name` for Font Awesome icons.
-     */
-    private string $style;
-
     public function setFixedWidth(bool $value): void
     {
         $this->fixedWidth = $value;
